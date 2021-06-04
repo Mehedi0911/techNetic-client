7bfeea8ea746058f78c86a96bb9a03bb02cc7c68
Mehedi0911 <mmraaj378@gmail.com>
2021-06-04 21:05:45 +0600

    added features

--- src/components/Home/Navbar/NavBar.js
+++ src/components/Home/Navbar/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import SerachBar from './SerachBar/SerachBar';
 import TopBar from './Topbar/TopBar';
 
 const NavBar = () => {
     return (
         <div>
             <TopBar></TopBar>
+            <SerachBar></SerachBar>
         </div>
     );
 };
--- src/components/Home/Navbar/SerachBar/SerachBar.js
+++ src/components/Home/Navbar/SerachBar/SerachBar.js
@@ -1,9 +1,31 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone, faRandom, faHeart, faShoppingBasket } from '@fortawesome/free-solid-svg-icons'
 const SerachBar = () => {
     return (
-        <div>
-            
+        <div className="bg-brand-dark p-2">
+            <div className="container d-flex justify-content-between align-items-center">
+                <div>
+                    <h5 className="brand-text-primary">TechNetic</h5>
+                </div>
+                <div style={{width:'300px'}}>
+                <input className="form-control" type="text" placeholder="search products"/>
+                </div>
+                <div style={{width:'250px'}} className="cart-summery d-flex justify-content-between brand-text-light"> 
+                    <div className="text-center">
+                        <FontAwesomeIcon icon={faRandom} />
+                        <p>Compare</p>
+                    </div>
+                    <div className="text-center">
+                        <FontAwesomeIcon icon={faHeart} />
+                        <p>Wishlist</p>
+                    </div>
+                    <div className="text-center">
+                        <FontAwesomeIcon icon={faShoppingBasket} />
+                        <p>Cart</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
